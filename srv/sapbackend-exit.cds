bbe7f4cedb413a481940e9d75ff031a34dfad204
mfgfiori <mfdezgarcia@gmail.com>
2023-02-24 19:23:08 +0000

    Before Fiori Elements

--- srv/sapbackend-exit.cds
+++ srv/sapbackend-exit.cds
@@ -6,5 +6,11 @@ define service SAPBackendExit {
         skip : false
     }
     @cds.autoexpose
-    entity Incidents as select from external.IncidentsSet;
+//    entity Incidents as select from external.IncidentsSet;
+    entity Incidents as projection on external.IncidentsSet;
+}
+
+@protocol: 'rest'
+service RestService{
+    entity Incidents as projection on SAPBackendExit.Incidents;
 }
--- srv/sapbackend-exit.js
+++ srv/sapbackend-exit.js
@@ -1,6 +1,6 @@
 const cds = require("@sap/cds");
 
-module.exports = cds.service.impl(async function() {
+/* module.exports = cds.service.impl(async function() {
     const { Incidents } = this.entities;
     const sapbackend = await cds.connect.to("sapbackend");
 
@@ -12,4 +12,35 @@ module.exports = cds.service.impl(async function() {
             }
         });
     });
-});
+}); */
+
+module.exports = async (srv) => {
+    const { Incidents } = srv.entities;
+    const sapbackend = await cds.connect.to("sapbackend");
+
+    srv.on(["READ"], Incidents, async (req) => {
+        let IncidentsQuery = SELECT.from(req.query.SELECT.from).limit(req.query.SELECT.limit);
+        if (req.query.SELECT.where) {
+            IncidentsQuery.where(req.query.SELECT.where);
+        };
+        if (req.query.SELECT.orderBy) {
+            IncidentsQuery.orderBy(req.query.SELECT.orderBy);
+        };
+
+        let incident = await sapbackend.tx(req).send({
+            query: IncidentsQuery,
+            headers: {
+                //Authorization: "Basic c2FwdWk1OnNhcHVpNQ==",
+                Authorization: `${process.env.SAP_GATEWAY_AUTH}`, 
+            },
+        });
+
+        let incidents = [];
+        if (Array.isArray(incident)) {
+            incidents = incident;
+        } else {
+            incidents[0] = incident;
+        }
+        return incidents;
+    });
+};
